8d293d0d211503258bce3b9753f808cab333f68e
Jon Veach <veach09@gmail.com>
2019-08-16 15:59:11 -0700

    Recommit

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.static('./public'));
 
 app.get('/', (request, response) => {
-  response.status(200).sendFile('./index.html');
+  // response.status(200).sendFile('./index.html');
+  response.status(200).sendFile(__dirname + '/index.html');
 })
 
 app.use('*', (request, response) => {
