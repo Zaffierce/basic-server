fd522e3be8ba10eac7e2de9f914845a125425c3f
Jon Veach <veach09@gmail.com>
2019-08-16 16:19:25 -0700

    Recommit the public!

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const e = require('express');
-const app = e();
+const express = require('express');
+require('dotenv').config();
+const app = express();
 
-app.use(e.static('./public'));
+app.use(express.static('./public'));
 
-app.listen(process.env.PORT || 3000);
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+app.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained'
+  }
+
+  response.status(200).json(airplanes);
+})
+
+app.get('/', (request, response) => {
+  response.status(200).redirect('index.html');
+})
+
+app.use('*', (request, response) => {
+  response.send('Sorry, that route does not exist');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
