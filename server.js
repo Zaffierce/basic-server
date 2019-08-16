f492b651198963ece580671c5037f37b24ae50bb
Jon Veach <veach09@gmail.com>
2019-08-16 16:03:40 -0700

    Recommit

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
-
-app.use(express.static('./public'));
-
-app.get('/', (request, response) => {
-  // response.status(200).sendFile('./index.html');
-  response.status(200).sendFile(__dirname + '/index.html');
-})
-
-app.use('*', (request, response) => {
-  response.send('Sorry, that route does not exist');
-})
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+const e = require('express');
+const app = e();
+​
+app.use(e.static('./public'));
+​
+app.listen(process.env.PORT);
