fa73751ccef9a01fa9888bbc717bc063af8c9168
Jon Veach <veach09@gmail.com>
2019-08-16 15:47:14 -0700

    Recommit

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-'use strict';
-
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const fs = require('fs');
 
-//When someone makes a request, move to this directory
 app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
-
-// app.get('/test', (request, response) => {
-//   const test = fs.readdir("./public", (err, files) => {
-//     if (err) return console.error(err);
-//     files.forEach(file => {
-//       let commandName = file.split(".")[0];
-//       console.log(`Attempting to load command ${commandName}`);
-//     });
-//   });
-//   response.status(200).send(test)
-// })
 
 app.get('/data', (request, response) => {
   let airplanes = {
@@ -29,18 +14,17 @@ app.get('/data', (request, response) => {
     canFly: true,
     pilot: 'Well Trained'
   }
-  response.status(200).json(airplanes);
-});
 
-app.get('/', (request, response) => { 
+  response.status(200).json(airplanes);
+})
+
+app.get('/', (request, response) => {
   response.status(200).redirect('index.html');
 })
 
-
-//This is the catch all if the directory does not exist
 app.use('*', (request, response) => {
-  response.send('Sorry, that route does not exist')
+  response.send('Sorry, that route does not exist');
 })
 
-const PORT = process.env.POR || 3000;
-app.listen(PORT, () => console.log(`Listening on ${PORT}.`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
